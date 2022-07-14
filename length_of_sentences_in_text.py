93b1a6cd87b49a224a3b4704195c839b2defbec5
ohjh064 <ohjh064>
2022-07-15 01:04:26 +0900

    leave only English and numbers

--- length_of_sentences_in_text.py
+++ length_of_sentences_in_text.py
@@ -2,9 +2,9 @@ import os
 import sys
 import re
 
-sys.stdout =open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\19-21_수능_문장_영어_전처리.txt", "w", encoding="utf-8")
+sys.stdout =open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\수능 문장모음\\2019-11-수능-문장-영어-전처리.txt", "w", encoding="utf-8")
 
-with open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\19-21_수능_문장_영어.txt","r", encoding="UTF-8") as file:
+with open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\수능 문장모음\\2019-11-수능-문장-영어.txt","r", encoding="UTF-8") as file:
     lines = file.readlines()
     for line in lines:
         a = line.replace(" ","")
@@ -15,14 +15,14 @@ sys.stdout.close()
 
 #as
 
-sys.stdout =open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\19-21_수능_문장_영어_문장길이.csv", "w", encoding="utf-8")
+sys.stdout =open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\수능 문장모음\\2019-11-수능-문장-영어.csv", "w", encoding="utf-8")
 
-with open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\19-21_수능_문장_영어_전처리.txt","r", encoding="UTF-8") as file2:
+with open("C:\\Users\\ohjh0\\OneDrive\\바탕 화면\\수능 문장모음\\2019-11-수능-문장-영어-전처리.txt","r", encoding="UTF-8") as file2:
     datas = file2.readlines()
     for data in datas:
         a = re.split('[?.!]', data)
         for i in a:
-            b = [j for j in i if not re.findall("[^\u0000-\u05C0\u2100-\u214F]+",j)]
+            b = re.sub('[^a-zA-Z0-9]','',i).strip()
             print(str(len(b)) + "," +"".join(b), end = "\n")
 
 sys.stdout.close()
